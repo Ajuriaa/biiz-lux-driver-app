8b21484845c0ed4f650d696a67f8e8e8afc68408
Roger! <icarus.gk@gmail.com>
2023-08-18 12:30:25 -0600

    add: AppHeader component and add it to HomePage

--- src/app/views/HomePage.vue
+++ src/app/views/HomePage.vue
@@ -1,58 +1,19 @@
 <script setup lang="ts">
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/vue';
 import { useRouter } from 'vue-router';
+import AppHeader from "@/components/AppHeader.vue";
 
 const router = useRouter();
 </script>
 
 <template>
   <IonPage>
-    <IonHeader :translucent="true">
-      <IonToolbar>
-        <IonTitle>Driver Home Page</IonTitle>
-      </IonToolbar>
-    </IonHeader>
-
     <IonContent :fullscreen="true">
-      <IonHeader collapse="condense">
-        <IonToolbar>
-          <IonTitle size="large">Blank</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-
-      <div id="container">
-        <strong>This will be the Driver Home Page</strong>
-     </div>
+      <AppHeader />
     </IonContent>
   </IonPage>
 </template>
 
-<style scoped>
-#container {
-  text-align: center;
-  
-  position: absolute;
-  left: 0;
-  right: 0;
-  top: 50%;
-  transform: translateY(-50%);
-}
+<style scoped lang="scss">
 
-#container strong {
-  font-size: 20px;
-  line-height: 26px;
-}
-
-#container p {
-  font-size: 16px;
-  line-height: 22px;
-  
-  color: #8c8c8c;
-  
-  margin: 0;
-}
-
-#container a {
-  text-decoration: none;
-}
 </style>
