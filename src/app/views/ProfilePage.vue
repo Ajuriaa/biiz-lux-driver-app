61adf36d36df8ead3ce07eec5ad3a59e1a4e66cf
Roger! <icarus.gk@gmail.com>
2023-08-18 12:28:44 -0600

    add: profile route and Page

---
+++ src/app/views/ProfilePage.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { IonPage, IonContent } from "@ionic/vue";
+import AppHeader from "@/components/AppHeader.vue";
+</script>
+
+<template>
+  <IonPage>
+    <IonContent :fullscreen="true">
+      <AppHeader />
+    </IonContent>
+  </IonPage>
+</template>
+
+<style scoped lang="scss">
+
+</style>
