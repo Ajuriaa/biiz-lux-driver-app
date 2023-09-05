100e2af0effb302085d3fcaf42b4d76e3e928815
Roger! <icarus.gk@gmail.com>
2023-09-04 18:45:09 -0600

    refactor: Maps into component

--- src/app/views/DemoMaps.vue
+++ src/app/views/DemoMaps.vue
@@ -1,46 +1,11 @@
 <script setup lang="ts">
-import { ref, onMounted, onUnmounted } from 'vue';
-import { IonPage } from '@ionic/vue';
-import { GoogleMap } from '@capacitor/google-maps';
-import { environment } from '../../environments/environments'; 
+import MapsBackground from '@/components/MapsBackground.vue';
 import PrimaryButton from '@/components/buttons/PrimaryButton.vue';
-
-const mapRef = ref();
-let newMap: GoogleMap;
-
-const tmpCoords = { lat: 14.060536, lng: -87.241214 };
-
-onMounted(async () => {
-  newMap = await GoogleMap.create({
-    id: 'my-map',
-    element: mapRef.value,
-    apiKey: environment.mapsApiKey,
-    config: {
-      center: tmpCoords,
-      zoom: 17,
-      clickableIcons: false,
-      disableDefaultUI: true,
-      keyboardShortcuts: false,
-      gestureHandling: "greedy"
-    },
-  });
-  
-  // Add a marker to the map
-  await newMap.addMarker({
-    coordinate: tmpCoords
-  });
-})
-
-// Clean up the map reference.
-onUnmounted(async () => await newMap.destroy());
 </script>
 
 <template>
   <IonPage>
-    <div
-      ref="mapRef"
-      class="maps-container"
-    />
+    <MapsBackground />
     <div class="buttons">
       <div class="action-buttons">
         <PrimaryButton>Eventos</PrimaryButton>
@@ -60,11 +25,6 @@ onUnmounted(async () => await newMap.destroy());
 </template>
 
 <style scoped lang="scss">
-.maps-container {
-  width: 100%;
-  height: 100%;
-}
-
 .buttons {
   position: absolute;
   bottom: 2rem;
