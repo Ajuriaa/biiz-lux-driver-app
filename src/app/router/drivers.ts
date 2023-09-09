df6d4f0fb6f5b142568763264d5d1eda9d5fa570
Roger! <icarus.gk@gmail.com>
2023-09-09 14:33:31 -0600

    remove: commented code

--- src/app/router/drivers.ts
+++ src/app/router/drivers.ts
@@ -1,29 +1,11 @@
 import { RouteRecordRaw } from 'vue-router';
-import { findToken } from '@/core/helpers/auth-helper';
 import DriverMainRouter from '@/components/driver/DriverMainRouter.vue';
 import HomePage from '@/views/HomePage.vue';
 import { Role } from '@/core/enums';
-import { getRole } from '@/core/helpers/role-helper';
 
 export const driverRoutes: Array<RouteRecordRaw> = [
   {
     path: '/driver',
-    beforeEnter: (to) => {
-      // reject the navigation
-      // when no token is found
-      // if (!findToken()) {
-      //   return { name: 'Login', query: { returnUrl: to.path } };
-      // }
-
-      // // when the role is invalid
-      // const currentRole = getRole();
-      // const urlRole = to.meta.role as string;
-      // const roleIsAllowed = currentRole.includes(urlRole);
-
-      // if (!roleIsAllowed) return { path: '/not-allowed' };
-
-      return true;
-    },
     component: DriverMainRouter,
     children: [
       {
