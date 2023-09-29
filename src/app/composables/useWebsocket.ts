4eb98dcee2e9bcf3538a40fe5d4702397d15380c
Roger! <icarus.gk@gmail.com>
2023-09-29 10:38:19 -0600

    fix: indenting on useWebsockets.ts

--- src/app/composables/useWebsocket.ts
+++ src/app/composables/useWebsocket.ts
@@ -1,63 +1,63 @@
-import { environment } from "../../environments/environments";
+import { environment } from '../../environments/environments';
 import { ref, onUnmounted } from 'vue';
-import { travelData, hasNewTrip } from "@/services/trip";
+import { travelData, hasNewTrip } from '@/services/trip';
 
 const driverId = 1;
 const chanelId = JSON.stringify({ channel: 'DriverCoordinatesChannel' });
 
 export function useWebsocket() {
-    const ws = new WebSocket(environment.wsUrl);
-    const wsStatus = ref(ws.readyState);
-    const coords = ref({ title: 'COORD', lat: 14.098533, lng: -87.226023 , driver: driverId });
+  const ws = new WebSocket(environment.wsUrl);
+  const wsStatus = ref(ws.readyState);
 
-    ws.addEventListener('open', () => {
-        wsStatus.value = ws.readyState;
-        ws.send(JSON.stringify({
-            command: 'subscribe',
-            identifier: chanelId
-        }));
-    });
+  const coords = ref({ title: 'COORD', lat: 14.098533, lng: -87.226023, driver: driverId });
 
-    ws.addEventListener('message', (event) => {
-        const data = JSON.parse(event.data);
+  ws.addEventListener('open', () => {
+    wsStatus.value = ws.readyState;
+    ws.send(
+      JSON.stringify({
+        command: 'subscribe',
+        identifier: chanelId,
+      }),
+    );
+  });
 
-        if (data.message === 'sendCoordinates') {
-            // Pa despues mi dog
-            // const { coords } = await Geolocation.getCurrentPosition();
+  ws.addEventListener('message', (event) => {
+    const data = JSON.parse(event.data);
 
-            const info = JSON.stringify({ action: 'send_coordinates', coords: coords.value });
-            const chanelId = JSON.stringify({ channel: 'DriverCoordinatesChannel' });
+    if (data.message === 'sendCoordinates') {
+      // Pa despues mi dog
+      // const { coords } = await Geolocation.getCurrentPosition();
 
-            console.log('sending coordinates', info);
+      const info = JSON.stringify({
+        action: 'send_coordinates',
+        coords: coords.value,
+      });
 
-            const payload = JSON.stringify({
-                command: 'message',
-                identifier: chanelId,
-                data: info
-            });
+      const payload = JSON.stringify({
+        command: 'message',
+        identifier: chanelId,
+        data: info,
+      });
 
-            ws.send(payload);
-        }
-
-        if (data.message && data.message.driver_id === driverId) {
-            hasNewTrip.value = true;
-            travelData.startCoords = data.message.start_coords;
-            travelData.endCoords = data.message.end_coords;
-            travelData.fare = data.message.fare;
-            travelData.passengerId = data.message.passenger_id;
-        }
-    });
-
-
-    const close = () => {
-        ws.close();
-        wsStatus.value = ws.readyState;
+      ws.send(payload);
     }
 
-    // Close the websocket
-    onUnmounted(() => {
-        close();
-    })
+    if (data.message && data.message.driver_id === driverId) {
+      hasNewTrip.value = true;
+      travelData.startCoords = data.message.start_coords;
+      travelData.endCoords = data.message.end_coords;
+      travelData.fare = data.message.fare;
+      travelData.passengerId = data.message.passenger_id;
+    }
+  });
 
-  return { ws }
+  const close = () => {
+    ws.close();
+    wsStatus.value = ws.readyState;
+  };
+
+  // Close the websocket
+  onUnmounted(() => close());
+
+  return { ws };
 }
