2398acddfaeb3af78ab48eaff7a551fcffffda3c
Ajuuu <alejoajuria@gmail.com>
2023-11-09 12:40:30 -0600

    angular project generated, vue stuff removed

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,15 +3,9 @@ import { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.atlantisdev.biizdriver',
   appName: 'BIIZ Driver',
-  webDir: 'dist',
+  webDir: 'dist/biiz-lux-driver-app',
   server: {
-    androidScheme: 'https',
-    // url: "http://192.168.0.7:5173/"
-  },
-  plugins: {
-    CapacitorCookies: {
-      enabled: true
-    }
+    androidScheme: 'http'
   }
 };
 
--- src/app/core/enums/marker.ts
+++
@@ -1,4 +0,0 @@
-export enum MarkerUrl {
-    driver = 'https://biiz-bucket.s3.us-east-2.amazonaws.com/driver-car.png',
-    passenger = 'https://biiz-bucket.s3.us-east-2.amazonaws.com/marker.png'
-}
